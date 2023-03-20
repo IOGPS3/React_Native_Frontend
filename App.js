e440d9f4aa5c58b886b34940038f4484a63dabf6
LaurenzPoll <laurenzvandepoll@icloud.com>
2023-03-20 14:45:29 +0100

    Setup of test

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>Welcome to React Native!</Text>
+                <Text style={styles.instructions}>
+                    This is a React Native snapshot test.
+                </Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+        flex: 1,
+        justifyContent: 'center',
+    },
+    instructions: {
+        color: '#333333',
+        marginBottom: 5,
+        textAlign: 'center',
+    },
+    welcome: {
+        fontSize: 20,
+        margin: 10,
+        textAlign: 'center',
+    },
 });
+
+export default App;

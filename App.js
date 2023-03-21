381e15c1db2b125e20fc0f11ac89c9428150bb22
MrTheRat <114981797+MrTheRat@users.noreply.github.com>
2023-03-21 11:45:04 +0100

    Update App.js
    app.js now shows a sectionlist based of first letter of each employee's name

--- App.js
+++ App.js
@@ -1,36 +1,94 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SectionList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-class App extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>Welcome to React Native!</Text>
-                <Text style={styles.instructions}>
-                    This is a React Native snapshot test.
-                </Text>
-            </View>
-        );
+const EmployeeList = () => {
+  const [employeeData] = useState([
+    { name: 'Alice', location: 'Floor 2' },
+    { name: 'Bob', location: 'Floor 1' },
+    { name: 'Charlie', location: 'Floor 2' },
+    { name: 'David', location: 'Home' },
+    { name: 'Eve', location: 'Floor 5' },
+    { name: 'Frank', location: 'Floor 2' },
+    { name: 'Grace', location: 'Floor 1' },
+    { name: 'Henry', location: 'Floor 3' },
+    { name: 'Isabella', location: 'Floor 2' },
+    { name: 'Jack', location: 'Home' },
+    { name: 'Kate', location: 'Floor 4' },
+    { name: 'Luke', location: 'Home' },
+  ]);
+  const [selectedEmployee, setSelectedEmployee] = useState(null);
+
+  const employeesByLetter = employeeData.reduce((acc, employee) => {
+    const firstLetter = employee.name.charAt(0);
+    if (!acc[firstLetter]) {
+      acc[firstLetter] = [];
     }
-}
+    acc[firstLetter].push(employee);
+    return acc;
+  }, {});
+
+  const sections = Object.keys(employeesByLetter)
+    .sort()
+    .map(letter => ({
+      title: letter,
+      data: employeesByLetter[letter],
+    }));
+
+  const handleEmployeePress = employee => {
+  const selectedEmployeeString = `${employee.name}, ${employee.location}`;
+  setSelectedEmployee(selectedEmployeeString);
+};
+
+  return (
+  <View style={styles.container}>
+  {selectedEmployee && (
+      <View style={styles.selectedEmployeeContainer}>
+        <Text style={styles.selectedEmployeeName}>
+          {selectedEmployee}
+        </Text>
+      </View>
+    )}
+    <SectionList
+      sections={sections}
+      keyExtractor={(item, index) => item.name + index}
+      renderItem={({ item }) => (
+        <TouchableOpacity onPress={() => handleEmployeePress(item)}>
+          <Text style={styles.item}>{item.name}</Text>
+        </TouchableOpacity>
+      )}
+      renderSectionHeader={({ section: { title } }) => (
+        <Text style={styles.header}>{title}</Text>
+      )}
+    />
+  </View>
+);
+};
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-        flex: 1,
-        justifyContent: 'center',
-    },
-    instructions: {
-        color: '#333333',
-        marginBottom: 5,
-        textAlign: 'center',
-    },
-    welcome: {
-        fontSize: 20,
-        margin: 10,
-        textAlign: 'center',
-    },
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  header: {
+    backgroundColor: '#f2f2f2',
+    fontSize: 16,
+    fontWeight: 'bold',
+    padding: 10,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  selectedEmployeeContainer: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: '#f2f2f2',
+  },
+  selectedEmployeeName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
 });
 
-export default App;
+export default EmployeeList;

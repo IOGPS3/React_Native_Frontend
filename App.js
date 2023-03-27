4f7bcfc73245cc3f9ae81a74e0bd596fcd99e038
MrTheRat <114981797+MrTheRat@users.noreply.github.com>
2023-03-27 13:20:32 +0200

    Update App.js
    added searchbar to main searching front-end

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
-import { SectionList, StyleSheet, Text, TouchableOpacity, View, TextInput } from 'react-native';
+import { SectionList, StyleSheet, Text, TouchableOpacity, View, Modal, Pressable, TextInput } from 'react-native';
 
+/**
+ * A component that displays a list of employees and allows the user to view their details.
+ *
+ * @returns {JSX.Element} - The EmployeeList component.
+ */
 const EmployeeList = () => {
+  // Define the initial employee data, selected employee, modal visibility, and search query using the `useState` hook.
   const [employeeData] = useState([
     { name: 'Alice', location: 'Floor 2' },
     { name: 'Bob', location: 'Floor 1' },
@@ -15,10 +21,19 @@ const EmployeeList = () => {
     { name: 'Jack', location: 'Home' },
     { name: 'Kate', location: 'Floor 4' },
     { name: 'Luke', location: 'Home' },
+    { name: 'Hank', location: 'Floor 5'},
   ]);
   const [selectedEmployee, setSelectedEmployee] = useState(null);
+  const [modalVisible, setModalVisible] = useState(true);
   const [searchQuery, setSearchQuery] = useState('');
 
+/**
+ * Groups an array of employees by the first letter of their first name.
+ *
+ * @param {array} employeeData - An array of employee objects.
+ *
+ * @returns {object} - An object containing employees grouped by the first letter of their first name.
+ */
   const employeesByLetter = employeeData.reduce((acc, employee) => {
     const firstLetter = employee.name.charAt(0);
     if (!acc[firstLetter]) {
@@ -34,35 +49,85 @@ const EmployeeList = () => {
     );
   });
 
+  /**
+ * Builds an array of sections for a list of employees, sorted alphabetically by first letter.
+ * Each section contains a title letter and an array of employees whose first name starts with that letter.
+ *
+ * @param {object} employeesByLetter - An object containing employees grouped by the first letter of their  first name.
+ *
+ * @returns {void} 
+ */
   const sections = Object.keys(employeesByLetter)
     .sort()
     .map(letter => ({
       title: letter,
-      data: employeesByLetter[letter].sort((a, b) => a.name.localeCompare(b.name)),
+      data: employeesByLetter[letter],
     }));
 
+/**
+ * Sets the selected employee and displays the employee details modal.
+ *
+ * @param {object} employee - The employee object to select.
+ *
+ * @returns {void}
+ */
   const handleEmployeePress = employee => {
-    const selectedEmployeeString = `${employee.name}, ${employee.location}`;
-    setSelectedEmployee(selectedEmployeeString);
+    setSelectedEmployee(employee);
+    setModalVisible(true);
   };
 
+  /**
+ * This component displays a list of employees with the ability to search for them and view details about a selected employee.
+ * 
+ * Props:
+ * - employees: an array of employee objects with properties "name" and "location"
+ * - onEmployeePress: a function that takes an employee object as an argument and is called when an employee is selected
+ * 
+ * State:
+ * - selectedEmployee: the employee object that is currently selected
+ * - searchQuery: the current search query entered by the user
+ * - modalVisible: a boolean that determines whether the modal for the selected employee is visible or not
+ * 
+ * @returns a View component containing a TextInput for searching, a SectionList for displaying the employee list, and a modal for displaying employee details.
+ */
   return (
-    <View style={styles.container}>
-      {selectedEmployee && (
-        <View style={styles.selectedEmployeeContainer}>
-          <Text style={styles.selectedEmployeeName}>
-            {selectedEmployee}
-          </Text>
-        </View>
-      )}
-      <TextInput
+  <View style={styles.container}>
+  {selectedEmployee && (
+      <View style={styles.selectedEmployeeContainer}>
+        <Text style={styles.selectedEmployeeName}>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert('Modal has been closed.');
+              setModalVisible(!modalVisible);
+            }}>
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>{selectedEmployee.name}</Text>
+                <Text style={styles.modalText}>{selectedEmployee.location}</Text>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}>
+                  <Text style={styles.textStyle}>Hide Modal</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+        </Text>
+      </View>
+    )}
+    {/* TextInput component to display the searchbar*/}
+    <TextInput
         style={styles.searchInput}
         value={searchQuery}
         onChangeText={query => setSearchQuery(query)}
         placeholder="Search employees"
       />
-      <SectionList
-        sections={filteredEmployees.length > 0 ? [{ data: filteredEmployees }] : sections}
+    {/* SectionList component to display the employee list */}
+    <SectionList
+      sections={filteredEmployees.length > 0 ? [{ data: filteredEmployees }] : sections}
         keyExtractor={(item, index) => item.name + index}
         renderItem={({ item }) => (
           <TouchableOpacity onPress={() => handleEmployeePress(item)}>
@@ -73,9 +138,9 @@ const EmployeeList = () => {
           <Text style={styles.header}>{title}</Text>
         )}
         extraData={searchQuery}
-      />
-    </View>
-  );
+    />
+  </View>
+);
 };
 
 const styles = StyleSheet.create({
@@ -103,14 +168,44 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
   },
-  searchInput: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 8,
-    paddingHorizontal: 12,
-    paddingVertical: 8,
-    marginHorizontal: 16,
-    marginBottom: 16,
+
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
   },
 });
 

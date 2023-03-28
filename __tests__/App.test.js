1947b95278aaddbf4074d3efa0d914040cf76b97
LaurenzPoll <laurenzvandepoll@icloud.com>
2023-03-28 13:16:10 +0200

    gitignore update

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,29 +1,89 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
-import App from '../App';
-import { getDatabase, ref, set, onValue } from 'firebase/database';
+import { render, fireEvent } from '@testing-library/react-native';
+import App from '../App.js';
+import { getDatabase, ref, onValue } from 'firebase/compat/database';
 
-jest.mock('firebase/database', () => ({
+
+jest.mock('firebase/compat/database', () => ({
     getDatabase: jest.fn(),
     ref: jest.fn(),
-    set: jest.fn(),
     onValue: jest.fn(),
 }));
 
-const mockDatabase = {
-    users: {},
-};
-
-beforeEach(() => {
-    getDatabase.mockImplementation(() => mockDatabase);
-    ref.mockImplementation((database, path) => path);
-    set.mockImplementation(() => Promise.resolve());
-    onValue.mockImplementation((_, callback) => {
-        callback({ val: () => mockDatabase.users });
+describe('EmployeeList', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
     });
-});
 
-test('renders EmployeeList with an empty list of employees inside App', () => {
-    const { getByText } = render(<App />);
-    expect(getByText('Search employees')).toBeTruthy();
+    it('should render the EmployeeList component', () => {
+        const { getByPlaceholderText, getByText } = render(<EmployeeList />);
+
+        expect(getByPlaceholderText('Search employees')).toBeTruthy();
+        expect(getByText('Search employees')).toBeTruthy();
+    });
+
+    it('should fetch data from Firebase', () => {
+        const databaseMock = {};
+        getDatabase.mockReturnValue(databaseMock);
+        ref.mockReturnValue('usersRef');
+        render(<EmployeeList />);
+
+        expect(getDatabase).toHaveBeenCalled();
+        expect(ref).toHaveBeenCalledWith(databaseMock, 'users');
+        expect(onValue).toHaveBeenCalledWith('usersRef', expect.any(Function));
+    });
+
+    it('should display employee data when available', () => {
+        const { getAllByText, getByText } = render(<EmployeeList />);
+
+        // Mock employee data
+        const employeeData = [
+            {
+                name: 'John Doe',
+                location: 'New York',
+            },
+            {
+                name: 'Jane Smith',
+                location: 'San Francisco',
+            },
+        ];
+
+        // Trigger onValue callback with employee data
+        onValue.mock.calls[0][1]({ val: () => employeeData });
+
+        // Check if employee names are rendered
+        expect(getAllByText(/John Doe|Jane Smith/)).toHaveLength(2);
+
+        // Check if section headers are rendered
+        expect(getByText('J')).toBeTruthy();
+    });
+
+    it('should filter employees based on search query', () => {
+        const { getByPlaceholderText, getAllByText, getByText } = render(<EmployeeList />);
+
+        // Mock employee data
+        const employeeData = [
+            {
+                name: 'John Doe',
+                location: 'New York',
+            },
+            {
+                name: 'Jane Smith',
+                location: 'San Francisco',
+            },
+        ];
+
+        // Trigger onValue callback with employee data
+        onValue.mock.calls[0][1]({ val: () => employeeData });
+
+        const searchInput = getByPlaceholderText('Search employees');
+
+        fireEvent.changeText(searchInput, 'John');
+
+        // Check if filtered employee name is rendered
+        expect(getByText('John Doe')).toBeTruthy();
+
+        // Check if non-matching employee name is not rendered
+        expect(() => getAllByText('Jane Smith')).toThrow();
+    });
 });

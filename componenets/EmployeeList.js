806b151ffccdb5ea35860660537d28a7bcd05e55
MrTheRat <114981797+MrTheRat@users.noreply.github.com>
2023-04-04 14:46:39 +0200

    added connection to api

--- componenets/EmployeeList.js
+++ componenets/EmployeeList.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
 import { SectionList, StyleSheet, Text, TouchableOpacity, View, Modal, Pressable, TextInput } from 'react-native';
+import { app } from '../firebaseConfig';
+import { getDatabase, ref, onValue } from 'firebase/database';
+
+//added for the <Icon /> element
+//install with command:
+//npm install @rneui/themed @rneui/base
+import { Icon } from '@rneui/base';
 
 //import styling
 import { styles } from '../Styling/components/EmployeeListStyle';
@@ -10,25 +17,38 @@ import { styles } from '../Styling/components/EmployeeListStyle';
  * @returns {JSX.Element} - The EmployeeList component.
  */
 const EmployeeList = () => {
-    // Define the initial employee data, selected employee, modal visibility, and search query using the `useState` hook.
-    const [employeeData] = useState([
-        { name: 'Alice', location: 'Floor 2' },
-        { name: 'Bob', location: 'Floor 1' },
-        { name: 'Charlie', location: 'Floor 2' },
-        { name: 'David', location: 'Home' },
-        { name: 'Eve', location: 'Floor 5' },
-        { name: 'Frank', location: 'Floor 2' },
-        { name: 'Grace', location: 'Floor 1' },
-        { name: 'Henry', location: 'Floor 3' },
-        { name: 'Isabella', location: 'Floor 2' },
-        { name: 'Jack', location: 'Home' },
-        { name: 'Kate', location: 'Floor 4' },
-        { name: 'Luke', location: 'Home' },
-        { name: 'Hank', location: 'Floor 5' },
-    ]);
+    // State variables for employee data, selected employee, modal visibility, and search query
+    const [employeeData, setEmployeeData] = useState([]);
     const [selectedEmployee, setSelectedEmployee] = useState(null);
-    const [modalVisible, setModalVisible] = useState(true);
+    const [modalVisible, setModalVisible] = useState(false);
     const [searchQuery, setSearchQuery] = useState('');
+
+    // Fetch data from Firebase Real-Time Database and update the employeeData state
+    useEffect(() => {
+        // Get a reference to the Firebase Real-Time Database
+        const database = getDatabase();
+
+        // Create a reference to the 'users' node
+        const usersRef = ref(database, 'users');
+
+        // Set up a listener for changes in the 'users' node
+        // The listener will be called with a snapshot of the data whenever it changes
+        const unsubscribe = onValue(usersRef, (snapshot) => {
+            // Extract the user data from the snapshot
+            const users = snapshot.val();
+
+            // Convert the user data object to an array of user objects
+            const formattedUsers = Object.values(users || {});
+
+            // Update the employeeData state with the fetched data
+            setEmployeeData(formattedUsers);
+        });
+
+        // Clean up the listener when the component is unmounted
+        return () => {
+            unsubscribe();
+        };
+    }, []);
 
     /**
      * Groups an array of employees by the first letter of their first name.
@@ -108,28 +128,30 @@ const EmployeeList = () => {
         <View style={styles.container}>
             {selectedEmployee && (
                 <View style={styles.selectedEmployeeContainer}>
-                    <Text style={styles.selectedEmployeeName}>
-                        <Modal
-                            animationType="slide"
-                            transparent={true}
-                            visible={modalVisible}
-                            onRequestClose={() => {
-                                Alert.alert('Modal has been closed.');
-                                setModalVisible(!modalVisible);
-                            }}>
-                            <View style={styles.centeredView}>
-                                <View style={styles.modalView}>
+                    <Modal
+                        animationType="slide"
+                        transparent={true}
+                        visible={modalVisible}
+                        onRequestClose={() => {
+                            //Alert.alert('Modal has been closed.');
+                            setModalVisible(!modalVisible);
+                        }}>
+                        <View style={styles.centeredView}>
+                            <View style={styles.modalView}>
+                                <View style={{ width: 300, Height: 600, flexDirection: 'row' }}>
+                                    <Icon reverse name='arrow-back' onPress={() => setModalVisible(!modalVisible)} />
                                     <Text style={styles.modalText}>{selectedEmployee.name}</Text>
-                                    <Text style={styles.modalText}>{selectedEmployee.location}</Text>
-                                    <Pressable
-                                        style={[styles.button, styles.buttonClose]}
-                                        onPress={() => setModalVisible(!modalVisible)}>
-                                        <Text style={styles.textStyle}>Hide Modal</Text>
-                                    </Pressable>
+                                    <Icon name='star' style={{ marginLeft: 30 }} />
                                 </View>
+                                <Text style={styles.modalText}>{selectedEmployee.location}</Text>
+                                <Pressable
+                                    style={[styles.button, styles.buttonClose]}
+                                    onPress={() => setModalVisible(!modalVisible)}>
+                                    <Text style={styles.textStyle}>Ping</Text>
+                                </Pressable>
                             </View>
-                        </Modal>
-                    </Text>
+                        </View>
+                    </Modal>
                 </View>
             )}
             {/* TextInput component to display the searchbar*/}

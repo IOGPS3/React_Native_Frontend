76753a51d28fde37d9aabfb31739fdc06f542924
LaurenzPoll <laurenzvandepoll@icloud.com>
2023-04-11 14:06:40 +0200

    Latest changes still not working correctrly

--- components/meetingSlider.js
+++ components/meetingSlider.js
@@ -4,10 +4,11 @@ import Slider from '@react-native-community/slider';
 import { styles } from '../Styling/components/MeetingSliderStyle';
 import { getDatabase, ref, onValue, off } from 'firebase/database';
 
-//const API_BASE_URL = 'http://localhost:3000'; // Replace with the API's base URL
+const API_BASE_URL = 'http://localhost:3000'; // Replace with the API's base URL
 
 const MeetingSlider = () => {
     const [sliderValue, setSliderValue] = useState(0);
+    const errorHandling = 'Error';
     const userId = 'user1'; // Replace with the desired user ID
 
     useEffect(() => {
@@ -31,30 +32,45 @@ const MeetingSlider = () => {
 
 
     // This code does not work. No api present at this moment. This route should be updated.
-    //const updateMeetingStatus = (value) => {
-    //    const status = value === 1 ? 'inMeeting' : 'available';
-    //    fetch(`${API_BASE_URL}/api/users/${userId}/meeting-status`, {
-    //        method: 'PUT',
-    //        headers: {
-    //            'Content-Type': 'application/json',
-    //        },
-    //        body: JSON.stringify({ meetingStatus: status }),
-    //    })
-    //        .then((response) => {
-    //            if (response.ok) {
-    //                setSliderValue(value);
-    //            } else {
-    //                throw new Error('Bad request');
-    //            }
-    //        })
-    //        .catch(error => {
-    //            Alert.alert('Error', 'Failed to update meeting status. Please try again.', [{ text: 'OK' }]);
-    //        });
-    //};
+    const updateMeetingStatus = (value) => {
+        const status = value === 1 ? 'inMeeting' : 'available';
+        fetch(`${API_BASE_URL}/api/users/${userId}/meeting-status`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ meetingStatus: status }),
+        })
+            //.then((response) => {
+            //    if (response.ok) {
+            //        setSliderValue(value);
+            //    } else {
+                    
+            //    }
+            //})
+            .catch(error => {
+                Alert.alert('Error', 'Failed to update meeting status. Please try again.', [{ text: 'OK' }]);
+                //sliderValue(0);
+                throw new Error('Bad request');
+            });
+    };
+
+    const Completing = (value) => {
+
+        if (errorHandling != "Error") {
+            //DONT TOUCH
+            setSliderValue(value);
+        }
+        else {
+            value(0);
+        }
+
+    };
 
     return (
         <View style={styles.container}>
             <Text style={styles.text}>{sliderValue === 1 ? 'In meeting' : 'Available'}</Text>
+            <Text style={styles.text}> slider value = {sliderValue} </Text>
             <Slider
                 style={styles.slider}
                 minimumValue={0}
@@ -64,6 +80,7 @@ const MeetingSlider = () => {
                 step={1}
                 value={sliderValue}
                 onValueChange={updateMeetingStatus}
+                onSlidingComplete={Completing}
             />
         </View>
     );

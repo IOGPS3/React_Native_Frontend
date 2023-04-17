53e1a7ed81951a6bdc9f8e457203aa6ab2787cb6
Robin van Hoof <robbie@vanhoof-erp.nl>
2023-04-17 18:22:24 +0200

    Android Channels Fix

--- App.js
+++ App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import EmployeeList from './components/EmployeeList';
 import MeetingSlider from './components/meetingSlider';
 import { styles } from './Styling/AppStyle';
-import { useEffect } from 'react';
 import { RegisterForPushNotification } from './components/Notification';
+
+import * as Notifications from 'expo-notifications';
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false      
+    }),
+});
 
 
 const App = () => {
+    const [expoPushToken, setExpoPushToken] = useState('');
+
     useEffect(() => {
-        RegisterForPushNotification()
-    });
+        RegisterForPushNotification().then(token => {
+            setExpoPushToken(token);
+            console.log(token);
+        })
+    }, []);
     return (
         <View style={styles.container}>
             <MeetingSlider />
--- components/Notification.js
+++ components/Notification.js
@@ -15,8 +15,36 @@ export const RegisterForPushNotification = async () => {
         return;
     }
 
-    token = (await Notifications.getExpoPushTokenAsync()).data;
-    console.log(token);
+    token = (await Notifications.getExpoPushTokenAsync({projectId: "dac6212c-ca63-4d33-bec4-443aaa56db22"})).data;
+
+    if (Platform.OS === 'android') {
+        Notifications.setNotificationChannelAsync('default', {
+          name: 'default',
+          importance: Notifications.AndroidImportance.HIGH,
+          vibrationPattern: [250, 0, 250, 0, 250],
+          lightColor: '#FF231F7C',
+        });
+      }
 
     return token;
 }
+
+export const SendPushNotification = async (expoPushToken) => {   
+    const message = {
+      to: expoPushToken,
+      sound: 'default',
+      title: 'Original Title',
+      body: 'And here is the body!',
+      data: { someData: 'goes here' },
+    };
+  
+    await fetch('https://exp.host/--/api/v2/push/send', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Accept-encoding': 'gzip, deflate',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(message),
+    });
+  }

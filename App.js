87536246116dbcd292a0351eedf4b645e7104025
Robin van Hoof <robbie@vanhoof-erp.nl>
2023-04-17 16:57:14 +0200

    Push Notifications Code

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
-import EmployeeList from './components/employeeList';
+import EmployeeList from './components/EmployeeList';
 import MeetingSlider from './components/meetingSlider';
 import { styles } from './Styling/AppStyle';
+import { useEffect } from 'react';
+import { RegisterForPushNotification } from './components/Notification';
 
 
 const App = () => {
+    useEffect(() => {
+        RegisterForPushNotification()
+    });
     return (
         <View style={styles.container}>
             <MeetingSlider />
--- components/Notification.js
+++ components/Notification.js
@@ -1,1 +1,22 @@
-// JavaScript source code
+import * as Notifications from 'expo-notifications';
+
+export const RegisterForPushNotification = async () => {
+    let token;
+
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+
+    if (existingStatus !== 'granted') {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+        alert('Failed to get push token for push notification!');
+        return;
+    }
+
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+
+    return token;
+}
